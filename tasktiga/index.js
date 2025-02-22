efefc81405c83a8624e8205d7d5a2895b3d96b94
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-02-22 20:51:33 +0700

    feat: add getUsersNameAndCity function to retrieve user data

---
+++ tasktiga/index.js
@@ -0,0 +1,19 @@
+const getUsers = async () => {
+  try {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    let arr = [];
+    const usersData = await response.json();
+    const resultData = usersData.map(({ name, address: { city } }) => {
+      arr.push({ name, city });
+    });
+    const sortCity = arr.sort((a, b) => a.city.localeCompare(b.city));
+    console.log(sortCity);
+  } catch (error) {
+    if (error instanceof Error) console.log(error);
+  }
+};
+getUsers();
