9d1d60b45f233f360d392d0b773e2340d3033b9f
mhakimsaputra17 <a1011201296@student.untan.ac.id>
2025-02-23 14:37:18 +0700

    feat(async): implement Promise-based data fetching function

---
+++ tasksatu/index.js
@@ -0,0 +1,40 @@
+function fetchData(status){
+  return new Promise(function(resolve, reject){
+    setTimeout(function (){
+      if(status ===true ){
+        resolve("Data berhasil diambil")
+      }
+      else{
+        reject("Gagal mengambil data")
+      }
+    },3000)
+  })
+}
+// contoh pemanggilan dengan status true
+fetchData(true).then((result)=>{
+  console.log("Sukses: ",result)
+}).catch((error)=>{
+  console.error("Gagal: ", error)
+});
+
+// contoh pemanggilan dengan status false
+fetchData(false).then((result)=>{
+  console.log("Sukses: ",result)
+}).catch((error)=>{
+  console.error("Gagal: ", error)
+});
+
+
+const AmbilData = async ()=>{
+  try {
+    const dataBerhasil = await fetchData(true);
+    console.log("Sukses Async:", dataBerhasil);
+    
+    const dataGagal = await fetchData(false)
+    console.log(dataGagal);
+
+  }catch (error) {
+    console.error("Gagal Async:", error);
+  }
+}
+AmbilData();
