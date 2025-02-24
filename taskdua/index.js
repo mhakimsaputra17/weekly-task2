64da014695ce5254594266ab507ab13bd478fdf1
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-02-24 09:31:58 +0700

    feat:add function getDataFromServer and add function processData

---
+++ taskdua/index.js
@@ -0,0 +1,24 @@
+function getDataFromServer(status, callback) {
+  setTimeout(function () {
+    if (status) {
+      callback(["product 1", "product 2", "product 3"], null);
+    } else {
+      const err = new Error("failed to fetch data");
+      err.name = "Error";
+      callback(null, err);
+    }
+  }, 3000);
+}
+
+const processData = (result, error) => {
+  try {
+    if (error) {
+      throw error;
+    }
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getDataFromServer(true, processData);
