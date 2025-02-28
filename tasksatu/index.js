7076c71b323163c027675ae3f73e1a67dc169056
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-02-28 15:40:45 +0700

    feat: add callFetch function and export their functions

--- tasksatu/index.js
+++ tasksatu/index.js
@@ -1,40 +1,45 @@
-function fetchData(status){
-  return new Promise(function(resolve, reject){
-    setTimeout(function (){
-      if(status ===true ){
-        resolve("Data berhasil diambil")
+function fetchData(status) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (status === true) {
+        resolve("Data berhasil diambil");
+      } else {
+        reject("Gagal mengambil data");
       }
-      else{
-        reject("Gagal mengambil data")
-      }
-    },3000)
-  })
+    }, 3000);
+  });
 }
-// contoh pemanggilan dengan status true
-fetchData(true).then((result)=>{
-  console.log("Sukses: ",result)
-}).catch((error)=>{
-  console.error("Gagal: ", error)
-});
 
-// contoh pemanggilan dengan status false
-fetchData(false).then((result)=>{
-  console.log("Sukses: ",result)
-}).catch((error)=>{
-  console.error("Gagal: ", error)
-});
+export function callFetch() {
+  // contoh pemanggilan dengan status true
+  fetchData(true)
+    .then((result) => {
+      console.log("Sukses: ", result);
+    })
+    .catch((error) => {
+      console.error("Gagal: ", error);
+    });
 
+  // contoh pemanggilan dengan status false
+  fetchData(false)
+    .then((result) => {
+      console.log("Sukses: ", result);
+    })
+    .catch((error) => {
+      console.error("Gagal: ", error);
+    });
+}
 
-const AmbilData = async ()=>{
+export const AmbilData = async () => {
   try {
     const dataBerhasil = await fetchData(true);
     console.log("Sukses Async:", dataBerhasil);
-    
-    const dataGagal = await fetchData(false)
-    console.log(dataGagal);
 
-  }catch (error) {
+    const dataGagal = await fetchData(false);
+    console.log(dataGagal);
+  } catch (error) {
     console.error("Gagal Async:", error);
   }
-}
-AmbilData();
+};
+// AmbilData();
+// export default AmbilData;
