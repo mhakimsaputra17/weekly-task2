778ae18e8cfa0a5bfe26d68b6a5962f951e09965
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-02-28 15:47:50 +0700

    feat:add export default funtion divideAndSort

--- tasklima/index.js
+++ tasklima/index.js
@@ -1,21 +1,23 @@
 const divideAndSort = (num) => {
-  if(typeof num !== "number")return "input harus number!"
+  if (typeof num !== "number") return "input harus number!";
   let numString = num.toString();
   let arrStrings = numString.split("0");
-  let newArrStrings = arrStrings.map(str => {
-    return str.split('').map(Number);
+  let newArrStrings = arrStrings.map((str) => {
+    return str.split("").map(Number);
   });
-  let sortedArr = newArrStrings.map(subArr => {
+  let sortedArr = newArrStrings.map((subArr) => {
     return subArr.sort((a, b) => a - b);
   });
   let flattenedStringArray = sortedArr.flat().map(String);
-  let finalString = flattenedStringArray.join('');
+  let finalString = flattenedStringArray.join("");
 
   let finalNumber = parseInt(finalString);
   return finalNumber;
-}
+};
 
-let numbers = 5956560159466056;
+// let numbers = 5956560159466056;
 
-console.log(divideAndSort(numbers));
-console.log(divideAndSort("satuduatiga"))
+// console.log(divideAndSort(numbers));
+// console.log(divideAndSort("satuduatiga"));
+
+export default divideAndSort;
