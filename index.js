e07cd2d30bd8ae869a38951c68c700e4da7432a8
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-02-28 15:51:36 +0700

    import all my function from my all taskfolder and run in index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import { AmbilData, callFetch } from "./tasksatu/index.js";
+import { getDataFromServer, processData } from "./taskdua/index.js";
+import getUsers from "./tasktiga/index.js";
+import { cekPalindrom, reverseWords } from "./taskempat/index.js";
+import divideAndSort from "./tasklima/index.js";
+import main from "./taskenam/interface.js";
+
+// Run Task 1
+// callFetch();
+// AmbilData();
+
+// Run Task 2
+// getDataFromServer(true, processData);
+
+// Run Task 3
+// getUsers();
+
+// Run Task 4
+// cekPalindrom("malam");
+// reverseWords("Saya Belajar Javascript");
+
+// Run Task 5
+// console.log(divideAndSort(5956560159466056));
+// console.log(divideAndSort("satuduatiga"));
+
+// Run Task 6
+main();
